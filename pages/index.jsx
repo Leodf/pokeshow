fd202da71be1daef89286b5228430a8615d55c28
Leonardo de Faveri <faver_i@hotmail.com>
2022-11-23 13:16:53 -0300

    estilizando os cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,7 @@
-
+import React from 'react'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
-
-import React from 'react'
+import Card from './components/Card'
 
 export async function getStaticProps() {
   const maxPokemons = 251
@@ -36,7 +35,7 @@ const index = ({ pokemons }) => {
       </div>
       <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <p key={pokemon.id}>{pokemon.name}</p>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>
     </>
