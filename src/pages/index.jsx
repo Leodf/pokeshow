527cfb6e971b62b3c70ce430ee2a736f881e2a54
Leonardo de Faveri <faver_i@hotmail.com>
2022-11-23 14:37:12 -0300

    utilizando o fallback do next

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,8 @@ import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import Card from '../components/Card'
 
+
+
 export async function getStaticProps() {
   const maxPokemons = 251
   const api = 'https://pokeapi.co/api/v2/pokemon/'
--- src/pages/pokemon/[pokemonId].jsx
+++ src/pages/pokemon/[pokemonId].jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/Pokemon.module.css'
 
+import { useRouter } from 'next/router'
+
 export const getStaticPaths = async () => {
     const maxPokemons = 251
     const api = 'https://pokeapi.co/api/v2/pokemon/'
@@ -17,7 +19,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false
+        fallback: true
     }
 }
 
@@ -28,49 +30,56 @@ export const getStaticProps = async (context) => {
     const data = await response.json()
 
     return {
-        props: { pokemon: data}
+        props: { pokemon: data }
     }
 }
 
 const Pokemon = ({ pokemon }) => {
-  return (
-    <div className={styles.pokemon_container}>
-        <h1 className={styles.title}>{pokemon.name}</h1>
-        <Image 
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-            width="200"
-            height="200"
-            alt={pokemon.name}
-            className={styles.pokemon_image}
-        />
-        <div>
-            <h3>Número:</h3>
-            <p>#{pokemon.id}</p>
-        </div>
-        <div className={styles.types_container}>
-            <h3>Tipo:</h3>
+
+    const router = useRouter()
+
+    if (router.isFallback) {
+        return <div>Carregando...</div>
+    }
+
+    return (
+        <div className={styles.pokemon_container}>
+            <h1 className={styles.title}>{pokemon.name}</h1>
+            <Image
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                width="200"
+                height="200"
+                alt={pokemon.name}
+                className={styles.pokemon_image}
+            />
             <div>
-                {pokemon.types.map((item, index) => (
-                    <span 
-                    key={index} 
-                    className={`${styles.type} ${styles['type_' + item.type.name]}`}
-                    >{item.type.name}</span>
-                ))}
+                <h3>Número:</h3>
+                <p>#{pokemon.id}</p>
             </div>
+            <div className={styles.types_container}>
+                <h3>Tipo:</h3>
+                <div>
+                    {pokemon.types.map((item, index) => (
+                        <span
+                            key={index}
+                            className={`${styles.type} ${styles['type_' + item.type.name]}`}
+                        >{item.type.name}</span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.data_container}>
+                <div className={styles.data_height}>
+                    <h4>Altura:</h4>
+                    <p>{pokemon.height * 10} cm</p>
+                </div>
+                <div className={styles.data_weight}>
+                    <h4>Peso:</h4>
+                    <p>{pokemon.weight / 10} Kg</p>
+                </div>
+            </div>
+
         </div>
-        <div className={styles.data_container}>
-            <div className={styles.data_height}>
-                <h4>Altura:</h4>
-                <p>{pokemon.height * 10} cm</p>
-            </div>
-            <div className={styles.data_weight}>
-                <h4>Peso:</h4>
-                <p>{pokemon.weight / 10} Kg</p>
-            </div>
-        </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Pokemon
