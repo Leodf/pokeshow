e220ed69b0eceeecc8d74de2c4cabc39d5ab049c
Leonardo de Faveri <faver_i@hotmail.com>
2022-11-23 12:28:54 -0300

    inserindo a chamada de api do pokeapi

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,34 @@ import styles from '../styles/Home.module.css'
 
 import React from 'react'
 
-const index = () => {
+export async function getStaticProps() {
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const response = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await response.json()
+  
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+}
+
+const index = ({ pokemons }) => {
   return (
-    <div>Pokeshow</div>
+    <div>
+      <h1>Pokeshow</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
